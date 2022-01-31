636d3f8c185edf6ba0d6fa856fbe72ee2bd5dadc
Aditya Deepak Mahale <ad619659@dal.ca>
2022-01-31 12:19:36 -0400

    Define routes

--- dms-ui/src/App.js
+++ dms-ui/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/login";
+import Register from "./components/register";
+import Recovery from "./components/recovery";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container pt-4">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recovery" element={<Recovery />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ dms-ui/src/components/recovery.jsx
@@ -0,0 +1,5 @@
+const Recovery = () => {
+  return <div>Recovery</div>;
+};
+
+export default Recovery;
