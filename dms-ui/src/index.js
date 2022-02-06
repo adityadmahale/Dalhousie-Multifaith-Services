49bf9c2477e621106927b2e2621aa70ea8b94713
Aditya Deepak Mahale <ad619659@dal.ca>
2022-02-06 09:54:49 -0400

    Apply CSS design

--- dms-ui/src/index.js
+++ dms-ui/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
+import "react-phone-number-input/style.css";
 import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
