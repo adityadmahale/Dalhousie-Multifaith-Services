f626602c8f4526b1226db72f72e82b372b74c204
Aditya Deepak Mahale <ad619659@dal.ca>
2022-02-13 01:09:02 -0400

    Add logo on login and register page

--- dms-ui/src/components/login.jsx
+++ dms-ui/src/components/login.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "./logo";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="form-layout text-center" onSubmit={handleSubmit}>
+      <Logo />
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+      <Link to="/recovery" className="link float-end">
+        Forgot Password?
+      </Link>
+      <button className="btn btn-primary">Sign In</button>
+      <div className="text-center">
+        <span>New to DMS? </span>
+        <Link to="/register" className="link">
+          Register
+        </Link>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
--- dms-ui/src/components/register.jsx
+++ dms-ui/src/components/register.jsx
@@ -1,5 +1,75 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PhoneInput from "react-phone-number-input";
+import Logo from "./logo";
+
 const Register = () => {
-    return ( <div>Register</div> );
-}
- 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(undefined);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="form-layout text-center" onSubmit={handleSubmit}>
+      <Logo />
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.currentTarget.value)}
+        />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.currentTarget.value)}
+        />
+      </div>
+      <PhoneInput
+        className="form-control"
+        value={phoneNumber}
+        onChange={setPhoneNumber}
+        placeholder="Phone Number"
+        defaultCountry="CA"
+      />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.currentTarget.value)}
+      />
+      <button className="btn btn-primary">Sign Up</button>
+      <div className="text-center">
+        <Link to="/login" className="link">
+          Already have an account?
+        </Link>
+      </div>
+    </form>
+  );
+};
+
 export default Register;
