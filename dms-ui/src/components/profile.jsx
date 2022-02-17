9df8d796a68b3372c0e139c548cffa42cf208118
Aditya Deepak Mahale <ad619659@dal.ca>
2022-02-17 19:34:11 -0400

    Add profile page content

--- dms-ui/src/components/profile.jsx
+++ dms-ui/src/components/profile.jsx
@@ -1,5 +1,38 @@
-const Profile = () => {
-  return <div className="profile-jumbotron">Profile</div>;
+const Profile = ({ user }) => {
+  return (
+    <div className="profile-jumbotron">
+      <div className="prl-img-container">
+        <img
+          className="profile-img"
+          src={process.env.PUBLIC_URL + "/andrew.png"}
+          alt="profile"
+        />
+      </div>
+      <div className="ri-pencil-fill ri-2x"></div>
+      <div className="row mt-5">
+        <div className="col-0 col-md-1"></div>
+        <div className="col-12 col-md-5">
+          <div className="label">First Name</div>
+          <div>{user.firstName}</div>
+        </div>
+        <div className="col-12 col-md-5">
+          <div className="label">Last Name</div>
+          <div>{user.lastName}</div>
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col-0 col-md-1"></div>
+        <div className="col-12 col-md-5">
+          <div className="label">Email</div>
+          <div>{user.email}</div>
+        </div>
+        <div className="col-12 col-md-5">
+          <div className="label">Contact Number</div>
+          <div>{user.contactNo}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
