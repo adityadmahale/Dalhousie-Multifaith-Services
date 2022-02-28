db2ec32e94b1a5a1050542856d7637fe3655205d
Aditya Deepak Mahale <ad619659@dal.ca>
2022-02-28 19:47:15 -0400

    Define serializers for JWT endpoint

--- dms/settings.py
+++ dms/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,11 +131,15 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1)
 }
 
 
 DJOSER = {
-    "SERIALIZERS": {"user_create": "core.serializers.UserCreateSerializer"}
+    "SERIALIZERS": {
+        "user_create": "core.serializers.UserCreateSerializer",
+        "current_user": "core.serializers.UserSerializer"
+    }
 }
 
 
