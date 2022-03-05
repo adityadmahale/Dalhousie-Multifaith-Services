fa0acaaf888caa44badcc6bc61f9c31589cce5a1
Aditya Deepak Mahale <ad619659@dal.ca>
2022-03-05 10:27:35 -0400

    Install django-heroku as a dependency

--- dms/settings/common.py
+++ dms/settings/common.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -126,6 +126,7 @@ DJOSER = {
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = "core.User"
 
--- dms/settings/prod.py
+++ dms/settings/prod.py
@@ -1,4 +1,6 @@
 import os
+import django_heroku
+
 from .common import * # NOQA
 
 DEBUG = False
@@ -6,3 +8,15 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ['DB_NAME'],
+        "HOST": os.environ['DB_HOST'], 
+        "USER": os.environ['DB_USER'], 
+        "PASSWORD": os.environ['DB_PASSWORD'],  
+    }
+}
+
+django_heroku.settings(locals())
