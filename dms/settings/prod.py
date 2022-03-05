6b271d38720b484fd7a94a661a85c3cf926b8c6a
Aditya Deepak Mahale <ad619659@dal.ca>
2022-03-05 10:28:22 -0400

    Correct flake8 errors

--- dms/settings/prod.py
+++ dms/settings/prod.py
@@ -13,9 +13,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.environ['DB_NAME'],
-        "HOST": os.environ['DB_HOST'], 
-        "USER": os.environ['DB_USER'], 
-        "PASSWORD": os.environ['DB_PASSWORD'],  
+        "HOST": os.environ['DB_HOST'],
+        "USER": os.environ['DB_USER'],
+        "PASSWORD": os.environ['DB_PASSWORD'],
     }
 }
 
