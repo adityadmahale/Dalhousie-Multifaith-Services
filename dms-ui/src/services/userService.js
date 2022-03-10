e0c4ac0ca8d104caa2a7c078e1e516063a37ed5b
Aditya Deepak Mahale <ad619659@dal.ca>
2022-03-10 10:44:20 -0400

    Include auth services

---
+++ dms-ui/src/services/userService.js
@@ -0,0 +1,30 @@
+import http from "./httpService";
+
+const authApiEndpoint = "/auth/users/";
+const dalUserApiEndpoint = "/dmsfront/dalusers/";
+const chaplainApiEndpoint = "/dmsfront/chaplains/";
+
+export function register(user) {
+  return http.post(authApiEndpoint, {
+    email: user.email,
+    first_name: user.first_name,
+    last_name: user.last_name,
+    password: user.password,
+  });
+}
+
+export function registerDalUser(user) {
+  return http.post(dalUserApiEndpoint, {
+    user_id: user.user_id,
+    phone: user.phone,
+  });
+}
+
+export function registerChaplain(user) {
+  return http.post(chaplainApiEndpoint, {
+    user_id: user.user_id,
+    phone: user.phone,
+    religion: user.religion,
+    description: user.description,
+  });
+}
