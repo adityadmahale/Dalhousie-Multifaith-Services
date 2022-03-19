35011b8e5b83c9c581f176f78f9ab3bd1b3eb2a5
Aditya Deepak Mahale <ad619659@dal.ca>
2022-03-19 11:42:32 -0300

    Fix questionnaire page

--- dms-ui/src/services/questions.js
+++ dms-ui/src/services/questions.js
@@ -2,58 +2,48 @@ const questions = [
   {
     id: "1",
     question_text: "I feel comfortable and at ease with my spiritual life.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
   {
     id: "2",
     question_text:
       "There is a direct relationship between my personal values and daily actions.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
   {
     id: "3",
     question_text:
       "When I get depressed or frustrated by problems, my spiritual beliefs and values give me direction.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
   {
     id: "4",
     question_text:
       "Prayer, meditation, and/or quiet personal reflection is/are important in my life.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
   {
     id: "5",
     question_text: "Life is meaningful for me, and I feel a purpose in life.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
   {
     id: "6",
     question_text:
       "I am able to speak comfortably about my personal values and beliefs.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
   {
     id: "7",
     question_text:
       "I am consistently striving to grow spiritually and I see it as a lifelong process.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
   {
     id: "8",
     question_text:
       "I have a strong sense of hope and optimism in my life and use my thoughts and attitudes in lifeaffirming ways.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
   {
     id: "9",
     question_text:
       "I appreciate the natural forces that exist in the universe.",
-    options: ["Very seldom", "Sometimes (occasionally)", "Almost always"],
   },
 ];
 
 export const getQuestions = () => {
   return questions;
 };
-
