bc62f1edb02c9c24f09e79da130196edd5ced1f0
Aditya Deepak Mahale <ad619659@dal.ca>
2022-03-19 11:45:40 -0300

    Add recovery endpoint

---
+++ core/views.py
@@ -0,0 +1,18 @@
+import json
+from rest_framework.views import Response
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import User
+
+
+@csrf_exempt
+@api_view(['PUT'])
+def recovery(request):
+    body = json.loads(request.body.decode('utf-8'))
+    user = User.objects.filter(email=body['email']).first()
+    if not user:
+        return Response({}, status=status.HTTP_400_BAD_REQUEST)
+    user.set_password(body['password'])
+    user.save()
+    return Response({}, status=status.HTTP_202_ACCEPTED)
