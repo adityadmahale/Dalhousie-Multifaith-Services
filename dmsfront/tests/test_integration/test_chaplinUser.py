8e98c0620506c5a2396ae1b24bbf858b16e48f6d
Harshit Lakhani <34265567+harshitlakhani@users.noreply.github.com>
2022-04-01 16:35:52 -0300

    smells resolved

--- dmsfront/tests/test_integration/test_chaplinUser.py
+++ dmsfront/tests/test_integration/test_chaplinUser.py
@@ -4,86 +4,48 @@ import pytest
 
 
 @pytest.mark.integration
-class TestChaplinAuth:
+class TestChaplinUsersAuth:
 
     url = "http://localhost:8000/auth/users/"
+    url = "http://localhost:8000/auth/users/"
     dal_user_url = "http://localhost:8000/dmsfront/dalusers/"
-    chaplin_url = "http://localhost:8000/dmsfront/chaplains/"
+    chaplin_url = "http://localhost:8000/dmsfront/dalusers/"
     events_url = "http://localhost:8000/dmsfront/"
     appointment_url = "http://localhost:8000/dmsfront/appointments/"
 
-    # ChaplainList-test adding chaplin with correct data
-    def test_chaplinList_post(self):
-        # user added
+    # ChaplinUser-test adding chaplin user with correct data
+    def test_ChaplinUser_post(self):
         user_response = requests.post(
             self.url,
             {
-                "email": "aditya@lakha.ca",
+                "email": "harshitshhcdsb@lakha.ca",
                 "password": "group@123",
-                "first_name": "aditya",
-                "last_name": "mahale",
-                "is_staff": True,
+                "first_name": "harshit",
+                "last_name": "lakhani",
+                "is_staff": False,
             },
         )
         id = user_response.json()["id"]
-        data = {
-            "user_id": id,
-            "phone": "9024538293",
-            "religion": "Hindu",
-            "description": "i am a chaplin",
-        }
-        # chaplin added
+        data = {"user_id": id, "phone": "9024538293", "user": "apexa"}
         response = requests.post(self.chaplin_url, data)
         assert response.status_code == status.HTTP_201_CREATED
 
-    # ChaplainList-test adding chaplin with correct data
-    def test_chaplinList_get(self):
+    # ChaplinUser-test adding chaplin user with correct data
+    def test_ChaplinUserDetail_get(self):
         # user added
         user_response = requests.post(
             self.url,
             {
-                "email": "kalpit@lakha.ca",
+                "email": "mvdfjnescbshdb@lakha.ca",
                 "password": "group@123",
-                "first_name": "kalpit",
-                "last_name": "mahale",
-                "is_staff": True,
+                "first_name": "harshit",
+                "last_name": "lakhani",
+                "is_staff": False,
             },
         )
         id = user_response.json()["id"]
-        data = {
-            "user_id": id,
-            "phone": "9024538293",
-            "religion": "Hindu",
-            "description": "i am a chaplin",
-        }
-        requests.post(self.chaplin_url, data)
-
-        # chaplin get by id
-        length = len(requests.get(self.chaplin_url).json())
-        assert length != 0
-
-    # ChaplainDetail-test getting chaplin detail with correct id
-    def test_chaplinDetail_get(self):
-        # user added
-        user_response = requests.post(
-            self.url,
-            {
-                "email": "anas@lakha.ca",
-                "password": "group@123",
-                "first_name": "anas",
-                "last_name": "mahale",
-                "is_staff": True,
-            },
-        )
-        id = user_response.json()["id"]
-        data = {
-            "user_id": id,
-            "phone": "9024538293",
-            "religion": "Hindu",
-            "description": "i am a chaplin",
-        }
-        # chaplin added
-        chaplin_response = requests.post(self.chaplin_url, data)
-
+        data = {"user_id": id, "phone": "9024538293", "user": "sneha"}
+        # daluser added
+        daluser_response = requests.post(self.chaplin_url, data)
         response = requests.get(self.chaplin_url + str(id))
-        assert response.json()["id"] == chaplin_response.json()["id"]
+        assert response.json()["id"] == daluser_response.json()["id"]
